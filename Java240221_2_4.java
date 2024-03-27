d716014289bfab7b40ef29823e78edc24abcd58e
jjjj0202 <165104793+jjjj0202@users.noreply.github.com>
2024-03-27 09:39:02 +0900

    test
    test

---
+++ Java240221_2_4.java
@@ -0,0 +1,19 @@
+package human.dc;
+
+public class Java240221_2_4 {
+
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 65;
+		
+		char c3 = '가';
+		char c4 = 44032;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+
+	}
+
+}
